f924f385372d0859b2bf7b0dc8253402929d18f8
Abhi <aabbhishekkumar@gmail.com>
2022-02-06 00:21:15 +0530

    commit

--- src/testpractise/TestPolymorphism.java
+++ src/testpractise/TestPolymorphism.java
@@ -20,6 +20,6 @@ System.out.println("SBI Rate of Interest: "+b.getRateOfInterest());
 b=new ICICI();  
 System.out.println("ICICI Rate of Interest: "+b.getRateOfInterest());  
 
-System.out.println("AXIS Rate of Interest: "+b.getRateOfInterest());  
+System.out.println("AXIS Rate of Interest:jhgjhggh "+b.getRateOfInterest());  
 }  
 }  
